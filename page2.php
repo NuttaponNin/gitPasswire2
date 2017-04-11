860f8b4e44bf716497942cb1dc61257c0d15e148
nuttapon <nuttapon.kmitl@gmail.com>
2017-04-11 12:30:39 +0700

    edit bug input resetPassword

---
+++ page2.php
@@ -0,0 +1,9 @@
+<?php
+$password = $_POST['password'];
+//$confirm_password = $_POST['confirm_pass'];
+echo "Password is ";
+echo ($password);
+echo "<br>";
+echo "Confirm Password is ";
+echo ($password);
+?>
