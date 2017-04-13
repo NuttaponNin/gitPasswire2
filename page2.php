4c9a16343b1fc540f13188cd174a43d64ecaacfa
nuttapon <nuttapon.kmitl@gmail.com>
2017-04-13 12:14:58 +0700

    edit bug input newPassword(1) on resetMasterPassword file, add tag input hidden one tag

--- page2.php
+++ page2.php
@@ -1,9 +1,13 @@
 <?php
 $password = $_POST['password'];
+$passHid = $_POST['passHid'];
 //$confirm_password = $_POST['confirm_pass'];
 echo "Password is ";
 echo ($password);
 echo "<br>";
 echo "Confirm Password is ";
 echo ($password);
+echo "<br>";
+echo "Password Hidden is ";
+echo ($passHid);
 ?>
